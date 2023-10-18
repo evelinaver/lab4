01c4f4ec723135921afe0d67eccf6281feb115d6
Evelina <evelina.ver11@gmail.com>
2023-10-18 14:29:17 +0300

    added lab 4 files

---
+++ src/main/java/Program.java
@@ -0,0 +1,7 @@
+public class Program {
+    public static void main(String[] args){
+        Numbers numbers = new Numbers();
+        int sum = numbers.sum( 5);
+        System.out.println("Sum from 0 till 5 = "+sum);
+    }
+}
